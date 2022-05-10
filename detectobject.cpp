f905705b3f645892a1f848b9357a0c2e5a1de4cd
quocan0303 <102713187+Deloidun@users.noreply.github.com>
2022-05-10 14:01:48 +0700

    add led control and pump control

--- detectobject.cpp
+++ detectobject.cpp
@@ -2,6 +2,9 @@
 const int trigPin = 9;
 const int echoPin = 10;
 const int vaultPin = 5;
+const int pumpPin = 4;
+const int fill_ledPin = 2;
+const int remove_ledPin = 7;
 
 
 // define variable
@@ -14,6 +17,9 @@ void setup(){
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(vaultPin, OUTPUT);
+  pinMode(pumpPin, OUTPUT);
+  pinMode(fill_ledPin, OUTPUT);
+  pinMode(remove_ledPin, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -36,16 +42,21 @@ void loop() {
 OnDistance = distance;
   
   // check if cup in a suitable distance to fill
-  if (OnDistance <= 10){
+  if (OnDistance <= 40){
       delay(2000);
       digitalWrite(vaultPin, HIGH);
+      digitalWrite(pumpPin, HIGH);
+      digitalWrite(fill_ledPin, HIGH);
       Serial.println("ON");
       Serial.println("filling the cup");
       delay(5000);
+      digitalWrite(fill_ledPin, LOW);	
       
       // while loop to check if the cup has been removed or not
-        while(OnDistance <= 10){
+        while(OnDistance <= 40){
             digitalWrite(vaultPin, LOW);
+            digitalWrite(pumpPin, LOW);
+            digitalWrite(remove_ledPin, HIGH);
             Serial.println("Please remove the cup");
           
             // Clears the trigPin
@@ -59,7 +70,9 @@ OnDistance = distance;
         }
     }    
     else{
+      digitalWrite(remove_ledPin, LOW);
       digitalWrite(vaultPin, LOW);
+      digitalWrite(pumpPin, LOW);
       Serial.println("OFF");
     }
   
